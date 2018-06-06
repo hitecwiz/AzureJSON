2090678a242ca8533b4a0896444ef5a954aa60fa
Ray Pearson <raymonp@microsoft.com>
2018-06-06 13:05:50 -0400

    Add DSC Example1 to repo

---
+++ DSCExample1.ps1
@@ -0,0 +1,19 @@
+﻿get-dscresource
+configuration DemoDSCConfiguration {
+    param($Value)
+       
+    node 'localhost' {
+        Environment 'EnvironmentResource' {
+            Name = 'Demo'
+            Value = $Value
+            }
+        }
+    }
+
+    DemoDSCConfiguration -Value 'DemoValue'
+
+    #Test Configuration
+    Test-DSCConfiguration -Path DemoDSCConfiguration
+
+    #Apply Configuration
+    Start-DSCConfiguration -Path DemoDSCConfiguration
